2a70353b3c4875109874b2b19d54ccb351dc4393
siham <siham.emf35@gmail.com>
2026-05-06 05:07:43 +0100

    implémenter IChannelRouter et ChannelRouterImpl

--- service-notification/src/main/java/ma/sgitu/g5/service/ChannelRouterImpl.java
+++ service-notification/src/main/java/ma/sgitu/g5/service/ChannelRouterImpl.java
@@ -1,5 +1,37 @@
 package ma.sgitu.g5.service;
 
-public class ChannelRouterImpl {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.sgitu.g5.dto.request.NotificationRequestDTO;
+import ma.sgitu.g5.dto.response.SendResultDTO;
+import ma.sgitu.g5.provider.IEmailProvider;
+import ma.sgitu.g5.provider.IPushProvider;
+import ma.sgitu.g5.provider.ISMSProvider;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChannelRouterImpl implements IChannelRouter {
+
+    private final IEmailProvider emailProvider;
+    private final ISMSProvider smsProvider;
+    private final IPushProvider pushProvider;
+
+    @Override
+    public SendResultDTO route(NotificationRequestDTO dto, String subject, String message) {
+        String channel = dto.getChannel();
+        log.info("[G5-ROUTER] Canal={} | notificationId={}", channel, dto.getNotificationId());
+
+        return switch (channel) {
+            case "EMAIL" -> emailProvider.send(
+                    dto.getRecipient().getEmail(), subject, message);
+            case "SMS" -> smsProvider.send(
+                    dto.getRecipient().getPhone(), message);
+            case "PUSH" -> pushProvider.send(
+                    dto.getRecipient().getDeviceToken(), message);
+            default -> throw new IllegalArgumentException(
+                    "[G5-ROUTER] Canal non supporte: " + channel);
+        };
+    }
 }
--- service-notification/src/main/java/ma/sgitu/g5/service/IChannelRouter.java
+++ service-notification/src/main/java/ma/sgitu/g5/service/IChannelRouter.java
@@ -1,5 +1,8 @@
 package ma.sgitu.g5.service;
 
-public interface IChannelRouter {
+import ma.sgitu.g5.dto.request.NotificationRequestDTO;
+import ma.sgitu.g5.dto.response.SendResultDTO;
 
+public interface IChannelRouter {
+    SendResultDTO route(NotificationRequestDTO dto, String subject, String message);
 }
